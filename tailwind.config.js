543bd36e714fd3ff6cc742e06cd558ecf211bcaa
kayodegeorge <gkayode75@gmail.com>
2022-07-03 22:11:24 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    sm:'480px',
+    md:'768px',
+    lg:'976px',
+    xl:'1440px',
+    extend: {},
+  },
+  plugins: [],
+}
